1b793bb354dfd8e0f1e33ea761ccad986b1315cb
makabaka1880 <makabaka1880@outlook.com>
2026-06-20 22:06:07 +0800

    feat: added github link

--- packages/site/src/components/NavBar.vue
+++ packages/site/src/components/NavBar.vue
@@ -3,6 +3,7 @@
         <span>
             <RouterLink to="/">Sewing Box</RouterLink>
         </span>
+        <a class="gh-link" href="https://github.com/makabaka1880/sewing-box" target="_blank" rel="noopener">GitHub</a>
     </nav>
 </template>
 
@@ -14,8 +15,21 @@
 @use "@/style/main.scss" as *;
 
 nav {
+    display: flex;
+    align-items: center;
     border-bottom: 1px solid var(--border);
     padding: 1rem var(--padding-safe);
+}
 
+.gh-link {
+    margin-left: auto;
+    color: var(--text-muted);
+    font-size: 0.85rem;
+    text-decoration: none;
+    transition: color 0.15s;
+
+    &:hover {
+        color: var(--text);
+    }
 }
 </style>
